8ad8ff5e09c6b01574da690970eb5b8e0cfed292
Abhi sahu <abhisahu12345@gmail.com>
2020-09-12 02:52:12 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ $conn= mysqli_connect('localhost','root','');
 mysqli_select_db($conn,"myblog");
 if($conn->connect_error) 
 {
-    echo  "Bhad me jao connect nhi ho raha h tumhara priya Database";
+    echo  "ERROR IN CONNECTING TO DB";
 
 }  
 
